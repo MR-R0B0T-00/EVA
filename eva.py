697aaa81c59aedd6005d4b0db20da3ed55d10af9
ptka4ev <ptka4ev@mail.ru>
2022-02-06 19:31:59 +0300

    Refactoring

--- eva.py
+++ eva.py
@@ -5,19 +5,19 @@ import speech_recognition as sr
 import pyttsx3
 import time
 import re
-import requests
 import datetime
 
 
-# Инициализация приложения
 eva = pyttsx3.init()
-voices = eva.getProperty('voices')
-# Выбор голоса системы, если ругается
-# Укажите 0 или 1
-eva.setProperty('voice', voices[0].id)
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
 
 
-# Функция для воспроизведения речи
+def set_voice():
+    eva.setProperty('rate', 200)
+    eva.setProperty('volume', 1)
+
+
 def speak(what):
     eva.say(what)
     eva.runAndWait()
@@ -26,67 +26,42 @@ def speak(what):
 
 # Обработка команд
 def commands(text):
-    # Погода, можно настроить свой город
-    # В конце ссылки просто поменять на нужный
-    if re.findall('погода|погоде|на улице|погодка', text):
-        print('О погоде')
-        weather = requests.get('https://yandex.ru/pogoda/astrahan')
-        bs = BeautifulSoup(weather.text, 'html.parser')
-        speak(bs.find('div', 'header-title header-title_in-fact').h1.text)
-        speak('Температура' + bs.find('div', 'temp fact__temp fact__temp_size_s').find('span', 'temp__value').text + 'градусов')
-        speak('Ощущается как' + bs.find('div', 'term term_orient_h fact__feels-like').find('span', 'temp__value').text)
-        speak(bs.find('div', 'link__condition day-anchor i-bem').text)
-        speak('Скорость ветра' + bs.find('span', 'wind-speed').text + 'метров в секунду')
     # Системное время
+    if text == 'ева':
+        speak('Слушаю, хозяин!')
     elif re.findall('время|времени|часы|часов', text):
-        print(f"Время {datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')}")
+        print(f">> Время {datetime.datetime.strftime(datetime.datetime.now(), '%H:%M')}")
         speak(datetime.datetime.strftime(datetime.datetime.now(), '%H %M'))
     # Завершение программы
     elif re.findall('пока|до свидания|отключись|стоп', text):
         answer = choice(['Всего доброго!', 'Пока!', 'До связи!'])
-        print(answer)
+        print(f'>> {answer}')
         speak(answer)
         sys.exit()
-    # Подключаемся к DialogFlow от Google
+    elif re.findall('привет|здарово|хай|здравствуй', text):
+        answer = choice(['Приветствую!', 'Привет!', 'Здравствуйте!'])
+        print(f'>> {answer}')
+        speak(answer)
     else:
-        # Вводим свой токен
-
-        request = apiai.ApiAI(CONFIG_SET['token']).text_request()
-        request.lang = 'ru'
-        request.session_id = 'EVA'
-        request.query = text
-        response = json.loads(request.getresponse().read().decode('utf-8'))
-        print(response)
-#        resp = response['result']['fulfillment']['speech']
-
-        if resp:
-            print(resp)
-            speak(resp)
-        else:
-            # Если ничего не вернулось
-            print('Не понятно')
-            speak('Я не совсем поняла!')
+        print(text)
+        speak(text)
 
 
-r = sr.Recognizer()
-m = sr.Microphone()
-
-
-def irina_run():
+def eva_run():
     while True:
         # Слушаем микрофон
-        with m as source:
-            print('Говорите, я слушаю.')
-            r.adjust_for_ambient_noise(source)
-            audio = r.listen(source)
+        with microphone as source:
+            print('>> Говорите, я слушаю.')
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
             try:
-                text = r.recognize_vosk(audio, language='ru_RU').lower()
+                text = recognizer.recognize_vosk(audio, language='ru_RU').lower()
                 commands(text)
             except sr.UnknownValueError:
                 # Если не разобрали
-                print('Я вас не понимаю, повторите!')
-                speak('Я вас не понимаю, повторите!')
+                speak('Повторите, пожалуйста!')
             time.sleep(0.1)
 
 
-irina_run()
+set_voice()
+eva_run()
